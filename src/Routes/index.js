7003b8f75c592328bf5e6b05aec4bd7b189ed620
TonyYou998 <19520931@gm.uit.edu.com>
2021-04-21 00:37:05 +0700

    add user page

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,6 @@
 import HomePage from "../Container/HomeTemplate/HomePage";
 import { UserPage } from "../Container/UserTemplate/UserPage";
+import { UserPageRemake } from "../Container/UserTemplate/UserPageRemake";
 const routesHome=[
     {
         exact:true,
@@ -14,7 +15,7 @@ const routesUser=[
     {
         exact:false,
         path:"/profile",
-        Component:UserPage,
+        Component:UserPageRemake,
     }
 ]
 
