33b8bec66c2dea6cf9eb34536387bedd01fc8a71
TonyYou998 <19520931@gm.uit.edu.com>
2021-04-23 20:52:24 +0700

    add group

---
+++ src/Container/UserTemplate/GroupsPage/Components/GroupItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function GroupItem() {
+    return (
+        <div className="group__item my-3 mx-4 btn btn-primary  col-3"> 
+            <h4>Web Design</h4>
+            <p style={{color:"black"}}>45 Members</p>
+            
+        </div>
+    )
+}
+
+export default GroupItem;
--- src/Container/UserTemplate/GroupsPage/index.js
+++ src/Container/UserTemplate/GroupsPage/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import CreateClassButton from './Components/CreateClassButton'
+import GroupItem from './Components/GroupItem'
+import JoinClassButton from './Components/JoinClassButton'
 
 function GroupsPage() {
+
+    
     return (
-        <div>
-            <div className="group__wrapper ">
-                <div className="container-fluid d-row">
-                    <div className="group-item col-6">A</div>
-                    <div className="group-item col-6">B</div>
-                    <div className="group-item col-6">C</div>
-                    <div className="group-item col-6">D</div>
-                </div>
-            </div>
-        </div>
+       
+           <div className="group-container d-flex">
+               <div className="d-row" >
+                    <GroupItem/>
+                    <GroupItem/>
+                     <GroupItem/>
+                      <GroupItem/>
+                      
+               </div>
+               
+                  <CreateClassButton/>
+                  <JoinClassButton/>
+                 
+           </div>
+       
     )
 }
 
