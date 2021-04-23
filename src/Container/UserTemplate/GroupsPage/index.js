d4f11714cc11b4642ec94b4ff3e74b838e7a0acf
TonyYou998 <19520931@gm.uit.edu.com>
2021-04-23 17:21:05 +0700

    backup100

--- src/Container/UserTemplate/GroupsPage/index.js
+++ src/Container/UserTemplate/GroupsPage/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function GroupsPage() {
     return (
-        <div style={{paddingLeft:"500px"}}>
-            group
+        <div>
+            <div className="group__wrapper ">
+                <div className="container-fluid d-row">
+                    <div className="group-item col-6">A</div>
+                    <div className="group-item col-6">B</div>
+                    <div className="group-item col-6">C</div>
+                    <div className="group-item col-6">D</div>
+                </div>
+            </div>
         </div>
     )
 }
