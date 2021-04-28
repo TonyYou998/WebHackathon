3f54873ab1b203674e508f0aad793b1ad0a86131
unknown <baoduong1011>
2021-04-28 22:12:40 +0700

    '28_4'

--- src/Container/UserTemplate/GroupsPage/GroupPage/Components/Question/index.js
+++ src/Container/UserTemplate/GroupsPage/GroupPage/Components/Question/index.js
@@ -1,123 +1,152 @@
 import { Button } from "@material-ui/core";
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import ListRead from "../ListRead";
+import Axios from 'axios';
+// import { resetState } from "sweetalert/typings/modules/state";
 function Question(props) {
-    const data=useSelector(state=>state.userReducer.data);
-      const handleCheck=()=>{
-          if(data && data.role==="gv"){
-            return (
-            //    <Button
-            //         className="col-4"
-            //         onClick={() => {
-            //           alert("Tính năng chưa phát triển");
-            //         }}
-            //       >
-            //         Kiểm tra
-            //       </Button>
-            // )
-            <Fragment>
-                <ListRead/>
-            </Fragment>
-          
-            )
-          }
-          else{
-            return ( <Button
-                    className="col-4"
-                    onClick={() => {
-                      alert("Tính năng chưa phát triển");
-                    }}
-                  >
-                    Chia sẻ
-                  </Button>)
-          }
-      }
+  const data = useSelector(state => state.userReducer.data);
+  const handleCheck = () => {
+    if (data && data.role === "gv") {
+      return (
+        //    <Button
+        //         className="col-4"
+        //         onClick={() => {
+        //           alert("Tính năng chưa phát triển");
+        //         }}
+        //       >
+        //         Kiểm tra
+        //       </Button>
+        // )
+        <Fragment>
+          <ListRead />
+        </Fragment>
 
-    return (
-              <>
-        <div className="col-12 mt-5"></div>
-        <div className="d-none d-md-block mt-3 mt-md-0 col-md-2 mr-md-5"></div>
-        <div className="col-12 col-md-9 shadow ml-0 ml-md-5 mt-5 mt-md-0">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="row">
-                  <div className="col-2 col-sm-2  col-md-1">
-                    <div
-                      className="avatar border bg-secondary"
-                      style={{ width: 50, height: 50, borderRadius: "50%" }}
-                    ></div>
-                  </div>
-                  <div className="col-7 col-sm-6 col-md-4 col-lg-2">
-                    <p className="font-weight-bold mb-0">Lâm Duy</p>
-                    <p className="">2 năm trước</p>
-                  </div>
+      )
+    }
+    else {
+      return (<Button
+        className="col-4"
+        onClick={() => {
+          alert("Tính năng chưa phát triển");
+        }}
+      >
+        Chia sẻ
+      </Button>)
+    }
+  }
+
+  const [state,setState] = useState({
+      video:'',
+      pdf:'',
+      moTa:''
+  })
+
+  let LayTaiLieuKhoaHoc = () => {
+      let promise = Axios({
+          url:'https://hackathon-be-dev.herokuapp.com/get',
+          method:'GET',
+          headers: {
+            "Content-Type": "application/json",
+          },
+      })
+      promise.then(res => {
+        console.log(res.data);
+      })
+
+      promise.catch(err => {
+        console.log(err.response.data);
+      })
+  }
+
+  useEffect(() => {
+      LayTaiLieuKhoaHoc();
+  },[])
+
+  return (
+    <Fragment>
+      <div className="col-12 mt-5"></div>
+      <div className="d-none d-md-block mt-3 mt-md-0 col-md-2 mr-md-5"></div>
+      <div className="col-12 col-md-9 shadow ml-0 ml-md-5 mt-5 mt-md-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="row">
+                <div className="col-2 col-sm-2  col-md-1">
+                  <div
+                    className="avatar border bg-secondary"
+                    style={{ width: 50, height: 50, borderRadius: "50%" }}
+                  ></div>
+                </div>
+                <div className="col-7 col-sm-6 col-md-4 col-lg-2">
+                  <p className="font-weight-bold mb-0">Lâm Duy</p>
+                  <p className="">2 năm trước</p>
                 </div>
               </div>
-              <div className="col-12">
-                <div className="container">
-                  <div>mọi người cho mình hỏi làm sao để routing từ home page qua contact vậy ạ</div>
-                </div>
+            </div>
+            <div className="col-12">
+              <div className="container">
+                <div>mọi người cho mình hỏi làm sao để routing từ home page qua contact vậy ạ</div>
               </div>
-              <div className="col-12">
-                <div className="row py-2 text-center">
-                  <Button className="col-4 like">
-                    <div className="interact">
-                      <div role="button" className="like-button mt-1">
-                        Like
+            </div>
+            <div className="col-12">
+              <div className="row py-2 text-center">
+                <Button className="col-4 like">
+                  <div className="interact">
+                    <div role="button" className="like-button mt-1">
+                      Like
                         <ul className="interact-options bg-light ">
-                          <li>
-                            <a href="#" className="like">
-                              👍🏽
+                        <li>
+                          <a href="#" className="like">
+                            👍🏽
                             </a>
-                          </li>
-                          <li>
-                            <a href="#" className="love">
-                              ❤️
+                        </li>
+                        <li>
+                          <a href="#" className="love">
+                            ❤️
                             </a>
-                          </li>
-                          <li>
-                            <a href="#" className="laugh">
-                              😆
+                        </li>
+                        <li>
+                          <a href="#" className="laugh">
+                            😆
                             </a>
-                          </li>
-                          <li>
-                            <a href="#" className="what">
-                              😯
+                        </li>
+                        <li>
+                          <a href="#" className="what">
+                            😯
                             </a>
-                          </li>
-                          <li>
-                            <a href="#" className="sad">
-                              😥
+                        </li>
+                        <li>
+                          <a href="#" className="sad">
+                            😥
                             </a>
-                          </li>
-                          <li>
-                            <a href="#" className="angry">
-                              😡
+                        </li>
+                        <li>
+                          <a href="#" className="angry">
+                            😡
                             </a>
-                          </li>
-                        </ul>
-                      </div>
+                        </li>
+                      </ul>
                     </div>
+                  </div>
+                </Button>
+                <Button
+                  className="col-4"
+                  onClick={() => {
+                    alert("Tính năng chưa phát triển");
+                  }}
+                >
+                  Bình luận
                   </Button>
-                  <Button
-                    className="col-4"
-                    onClick={() => {
-                      alert("Tính năng chưa phát triển");
-                    }}
-                  >
-                    Bình luận
-                  </Button>
-                 
-                  {handleCheck()}
-                </div>
+
+                {handleCheck()}
               </div>
             </div>
           </div>
         </div>
-      </>
-    )
+      </div>
+    </Fragment>
+  )
 }
 
 export default Question
