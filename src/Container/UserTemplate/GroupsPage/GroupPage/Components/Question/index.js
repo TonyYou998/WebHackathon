72ba6d05a697092ea131d6e700f609e12ed890a7
TonyYou998 <19520931@gm.uit.edu.com>
2021-04-29 13:16:23 +0700

    present

--- src/Container/UserTemplate/GroupsPage/GroupPage/Components/Question/index.js
+++ src/Container/UserTemplate/GroupsPage/GroupPage/Components/Question/index.js
@@ -36,11 +36,27 @@ function Question(props) {
     }
   }
 
-  const [state,setState] = useState({
-      video:'',
-      pdf:'',
-      moTa:''
+  const [state, setState] = useState({
+    video: '',
+    pdf: '',
+    moTa: '',
+    flag: false,
+    autoplay: 0,
+    timeOut: 10000,
+    True: false,
+    ChuaXong: false,
+    key: false
   })
+
+  let runTime = () => {
+    let time = 0;
+    while (true) {
+      setTimeout(() => {
+        time++;
+      }, 1000)
+      if (time === 10) break;
+    }
+  }
 
   let LayTaiLieuKhoaHoc = () => {
       let promise = Axios({
@@ -60,12 +76,67 @@ function Question(props) {
     })
   }
 
+  let GuiDuLieuSinhVienHoanThanh = () => {
+
+  }
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   useEffect(() => {
-      LayTaiLieuKhoaHoc();
-  },[])
+    LayTaiLieuKhoaHoc();
+  }, [])
+
+  let RunTime = () => {
+    setTimeout(() => {
+      alert("ROI");
+    }, 3000)
+  }
+
+  // useEffect(() => {
+  //   if(state.True===true) {
+
+  //       alert("ROI");
+  //   }
+  //   else {
+  //     // clearTimeout();
+  //     alert("CHUA");
+  //   }
+  // },[state.True])
+
+
+  // useEffect(() => {
+  //   var myVar;
+  //   function Run_Time() {
+  //   myVar = setTimeout(function(){ alert("Hello"); }, 3000);
+  //   setTimeout(function(){ alert("Hello"); }, 3000)
+  //   }
+
+  //   function StopF() {
+  //     clearTimeout(myVar);
+  //   }
+
+  //   if(state.True===true) {
+  //     // setState({...state,True:false});
+  //     Run_Time();
+  //     StopF();  
+
+  //   }
+  //   else {
+  //     setTimeout(() => {
+
+  //     },3000)
+
+  //     // setState({...state,True:false});
+  //   }
+  // },[state.autoplay])
+
+  console.log("True:", state.True);
+  console.log("autoPlay:", state.autoplay);
 
   return (
-    <Fragment>
+    <div className='question__BD'>
       <div className="col-12 mt-5"></div>
       <div className="d-none d-md-block mt-3 mt-md-0 col-md-2 mr-md-5"></div>
       <div className="col-12 col-md-9 shadow ml-0 ml-md-5 mt-5 mt-md-0">
@@ -146,7 +217,152 @@ function Question(props) {
           </div>
         </div>
       </div>
-    </Fragment>
+
+
+
+
+      <div className="col-12 mt-5"></div>
+      <div className="d-none d-md-block mt-3 mt-md-0 col-md-2 mr-md-5"></div>
+      <div className="col-12 col-md-9 shadow ml-0 ml-md-5 mt-5 mt-md-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="row">
+                <div className="col-2 col-sm-2  col-md-1">
+                  <div
+                    className="avatar border bg-secondary"
+                    style={{ width: 50, height: 50, borderRadius: "50%" }}
+                  ></div>
+                </div>
+                <div className="col-7 col-sm-6 col-md-4 col-lg-2">
+                  <p className="font-weight-bold mb-0">Lâm Duy</p>
+                  <p className="">2 năm trước</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="container">
+                <div className='video__Display'>
+
+                </div>
+
+                <div className='button__BD'>
+                  <button onClick={() => {
+                    setState({ autoplay: 1 });
+                    setTimeout(() => {
+                      setState({ True: true, key: true, autoplay: 1 })
+                    }, 8000)
+                  }} className='btnBD m-4' type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">XEM VIDEO</button>
+                </div>
+
+                <div closeButton show={show}
+                  onHide={handleClose}
+                  backdrop="static"
+                  keyboard={false} style={{ backgroundColor: 'black', }}>
+                  <div show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}  style={{ backgroundColor: 'black', }} className="modal fade" id="exampleModalLong" tabIndex={1} role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                    <div show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false} style={{ maxWidth: '800px', width: '800px' }} className="modal-dialog" role="document">
+                      <div  className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title text-center" id="exampleModalLongTitle">Modal title</h5>
+                          {state.key ? <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                          </button> : <button disabled style={{ cursor: 'not-allowed' }} type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                          </button>}
+                        </div>
+                        <div className="modal-body">
+                          <iframe onClick={() => {
+                            console.log("HELLO");
+                          }} autoplay="1" id="videoDisplay" width="760" height="415" src={`https://www.youtube.com/embed/jO2viLEW-1A?autoplay=${state.autoplay}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div className="modal-footer">
+                          {state.key ? <button onClick={() => {
+                            setState({ autoplay: 0, True: false });
+                          }} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button> : <button disabled style={{ cursor: 'not-allowed' }} onClick={() => {
+                            setState({ autoplay: 0, True: false });
+                          }} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+              </div>
+
+
+            </div>
+            <div className="col-12">
+              <div className="row py-2 text-center">
+                <Button className="col-4 like">
+                  <div className="interact">
+                    <div role="button" className="like-button mt-1">
+                      Like
+                        <ul className="interact-options bg-light ">
+                        <li>
+                          <a href="#" className="like">
+                            👍🏽
+                            </a>
+                        </li>
+                        <li>
+                          <a href="#" className="love">
+                            ❤️
+                            </a>
+                        </li>
+                        <li>
+                          <a href="#" className="laugh">
+                            😆
+                            </a>
+                        </li>
+                        <li>
+                          <a href="#" className="what">
+                            😯
+                            </a>
+                        </li>
+                        <li>
+                          <a href="#" className="sad">
+                            😥
+                            </a>
+                        </li>
+                        <li>
+                          <a href="#" className="angry">
+                            😡
+                            </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </Button>
+                <Button
+                  className="col-4"
+                  onClick={() => {
+                    alert("Tính năng chưa phát triển");
+                  }}
+                >
+                  Bình luận
+                  </Button>
+
+                {handleCheck()}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+    </div>
   )
 }
 
