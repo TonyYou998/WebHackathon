6021db4ef78d179502c4f98413ad6a6ddfe64274
TonyYou998 <19520931@gm.uit.edu.com>
2021-04-29 15:45:20 +0700

    demo truoc khi chet

--- src/Container/UserTemplate/GroupsPage/Components/GroupItem/index.js
+++ src/Container/UserTemplate/GroupsPage/Components/GroupItem/index.js
@@ -6,8 +6,8 @@ function GroupItem(props) {
     const {data}=props;
 
     return (
-        <Link to="/user/groupid"className="group__item my-3 mx-4 btn  shadow col-xl-2 col-lg-3 col-md-4 col-sm-4 col-10"> 
-            <h4 style={{overflow:"hidden"}}>{data.ten}</h4>
+        <Link  style={{overflow:"hidden"}} to="/user/groupid"className="group__item my-3 mx-4 btn  shadow col-xl-2 col-lg-3 col-md-4 col-sm-4 col-10"> 
+            <h4>{data.ten}</h4>
             {/* <p style={{color:"black"}}>{data.siSo} Thành Viên</p> */}
             <hr/>
             <hr/>
